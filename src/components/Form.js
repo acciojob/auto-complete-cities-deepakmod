c7131b5d3c7c6049177fc2e6bb43cd130c8cfebc
Deepak Modanwal <deepakmodanwal404@gmail.com>
2023-06-18 11:25:32 +0530

    next

--- src/components/Form.js
+++ src/components/Form.js
@@ -25,11 +25,11 @@ function Form( {cities} ){
         <div>
             <p>Search cities of india:</p>
             <input value={values} type='text' onChange={(e)=>{search(e.target.value)}} ></input>
-            <div>
+            <ul>
                 {filter.map((city)=>{
-                    return <div onClick={(e)=>{displauInput(e.target.innerText)}}>{city}</div>
+                    return <li onClick={(e)=>{displauInput(e.target.innerText)}}>{city}</li>
                 })}
-            </div>
+            </ul>
         </div>
     )
 }
